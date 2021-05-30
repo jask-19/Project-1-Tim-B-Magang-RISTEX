0e95c9fb99d52cadfcbebb923a20d18fba9fab1e
Karina Widya Ramadhani <80022868+karinaara@users.noreply.github.com>
2021-05-30 08:50:50 +0700

    Delete views.py

--- tugasmagang1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def profil(request):
-    context={
-        'judul':'profil'
-    }
-    return render(request,'profil.html',context)
